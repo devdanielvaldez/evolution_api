84da563bc0bd6f6887793fb161aadd4e69d2b7a1
Daniel Valdez | FullStack Developer <hello@daniel-valdez.com>
2025-03-10 15:44:23 -0400

    fix

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ app.post('/activate', (req, res) => {
 app.get('/is-active/:email', (req, res) => {
     const email = req.params.email;
 
-    if (email == 'gruposerulle@gmail.com') return res.status(200).json({ isActive: true });
+    if (email == 'gruposerulle@gmail.com' || email == 'pascualulloa@gmail.com') return res.status(200).json({ isActive: true });
 
     if (!fs.existsSync(JSON_FILE_PATH)) {
         return res.status(404).json({ error: 'No hay datos almacenados' });
